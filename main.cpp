70cd06e84e3c767e90a641fa1ac0fcf75b3c74af
welove-t <tlove.tky@gmail.com>
2023-12-02 11:38:28 +0900

    socketの作成

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+
+const int PORT = 8080;
+const int BACKLOG = 10;
+const int MAX_CONNECTIONS = 5;
+
+int create_socket()
+{
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1)
+    {
+        std::cerr << "ソケットの作成に失敗しました。" << std::endl;
+        return -1;
+    }
+    return sockfd;
+}
+
+bool set_socket_options(int sockfd)
+{
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+        std::cerr << "ソケットオプションの設定に失敗しました。" << std::endl;
+        return false;
+    }
+    return true;
+}
+
+bool bind_and_listen(int sockfd)
+{
+    struct sockaddr_in addr;
+    std::memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        std::cerr << "バインドに失敗しました。" << std::endl;
+        return false;
+    }
+
+    if (listen(sockfd, BACKLOG) < 0)
+    {
+        std::cerr << "リスニングに失敗しました。" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int sockfd = create_socket();
+    if (sockfd < 0)
+        return -1;
+
+    if (!set_socket_options(sockfd))
+    {
+        close(sockfd);
+        return -1;
+    }
+
+    if (!bind_and_listen(sockfd))
+    {
+        close(sockfd);
+        return -1;
+    }
+
+    std::cout << "サーバーが起動し、ポート" << PORT << "で接続を待っています。" << std::endl;
+
+    while (true)
+    {
+        int client_sockfd = accept(sockfd, NULL, NULL);
+        if (client_sockfd < 0)
+        {
+            std::cerr << "クライアントからの接続受付に失敗しました。" << std::endl;
+            continue;
+        }
+
+        // クライアントとの通信処理（省略）
+
+        close(client_sockfd);
+    }
+
+    close(sockfd);
+    return 0;
+}
