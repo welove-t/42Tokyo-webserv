14e17490489f79de24cafc4cde7173a904d508c8
love <tlove.tky@gmail.com>
2023-12-09 15:57:43 +0900

    HTTPレスポンスの生成処理を追加

--- request_handler.cpp
+++ request_handler.cpp
@@ -56,40 +56,35 @@ void parse_request(const std::string &request) {
     }
 }
 
+// HTTPレスポンスの生成
+std::string create_http_response(const std::string& status, const std::string& content, const std::string& content_type = "text/html") {
+    std::string response = "HTTP/1.1 " + status + "\r\n";
+    response += "Content-Length: " + std::to_string(content.size()) + "\r\n";
+    response += "Content-Type: " + content_type + "\r\n";
+    response += "\r\n";
+    response += content;
+    return response;
+}
+
 // HTTPリクエストを処理する関数
 void handle_request(const std::string& method, const std::string& uri, const std::map<std::string, std::string>& headers, const std::string& body) {
     std::string response;
     if (method == "GET") {
-        // 静的ファイルのパスを決定（例: "static/" + uri）
-        std::string file_path = "static" + uri;  // "static"は静的ファイルが格納されているディレクトリ
-
-        // ファイルのオープン
+        std::string file_path = "static" + uri;
         std::ifstream file(file_path.c_str(), std::ios::in | std::ios::binary);
         if (file) {
-            // ファイル内容を読み込む
             std::ostringstream file_content;
             file_content << file.rdbuf();
             file.close();
-
-            // レスポンスの生成
-            response = "HTTP/1.1 200 OK\r\n";
-            response += "Content-Length: " + std::to_string(file_content.str().size()) + "\r\n";
-            response += "Content-Type: text/html\r\n";  // 適切なContent-Typeを設定
-            response += "\r\n";
-            response += file_content.str();
+            response = create_http_response("200 OK", file_content.str());
         } else {
-            // ファイルが見つからない場合のエラーレスポンス
-            response = "HTTP/1.1 404 Not Found\r\n";
-            response += "Content-Length: 0\r\n";
-            response += "\r\n";
+            response = create_http_response("404 Not Found", "", "text/plain");
         }
     } else if (method == "POST") {
         // POSTリクエストの処理
-        // フォームデータの受け取り、APIエンドポイントへの応答など
     } else if (method == "DELETE") {
-    // その他のHTTPメソッドに対する処理もここに追加
-
-    // レスポンスの生成と送信
-    // std::string response = "HTTP/1.1 200 OK\r\nContent-Length: ... \r\n\r\n[レスポンスボディ]";
-    // レスポンスの送信
+        // DELETEリクエストの処理
+    }
+    // レスポンスの送信（例: send関数の使用）
 }
+
